4723e309f8ebcc22f295de9e18f7d934623c74d0
Kartik-Walia <itzkartikeds3@gmail.com>
2025-01-23 13:13:58 +0530

    Added dummy data for products

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from "./components/Header/Header";
+
 function App() {
   return (
     <div>
-      My React Store
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>React Store</h1>
+        </div>
+    )
+}
+
+export default Header;
