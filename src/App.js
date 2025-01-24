74911b7df17b37fbd186c27e68460b8a63c79183
Kartik-Walia <itzkartikeds3@gmail.com>
2025-01-25 02:19:30 +0530

    Added Cart Quantity Management and Resolved error of duplicate items in cart (learned concept of filter, findIndex, conditional rendering, passing parameters from child to parent, to a function which was declared in parent)

--- src/App.js
+++ src/App.js
@@ -11,20 +11,61 @@ function App() {
   const closeCart = () => setShowCart(false);
 
   const handleAddToCart = (productId, productName, productImage) => {
-    const cartItem = {
-      id: productId,
-      name: productName,
-      image: productImage,
-      quantity: 1,
-    };
-    setCartItems((state) => [...state, cartItem]);
+    const productInCartIndex = cartItems.findIndex(
+      (item) => item.id === productId
+    );
+
+    if (productInCartIndex === -1) {
+      const cartItem = {
+        id: productId,
+        name: productName,
+        image: productImage,
+        quantity: 1,
+      };
+      setCartItems((state) => [...state, cartItem]);
+    } else {
+      const updatedCartItems = [...cartItems];
+      updatedCartItems[productInCartIndex].quantity += 1;
+      setCartItems(updatedCartItems);
+    }
+  };
+
+  const handleIncreaseQuantity = (productId) => {
+    const productInCartIndex = cartItems.findIndex(
+      (item) => item.id === productId
+    );
+    const updatedCartItems = [...cartItems];
+    updatedCartItems[productInCartIndex].quantity += 1;
+    setCartItems(updatedCartItems);
+  };
+
+  const handleDecreaseQuantity = (productId) => {
+    const productInCartIndex = cartItems.findIndex(
+      (item) => item.id === productId
+    );
+    const qty = cartItems[productInCartIndex].quantity;
+    let updatedCartItems = [...cartItems];
+    if (qty === 1) {
+      updatedCartItems = updatedCartItems.filter(
+        (item, index) => index !== productInCartIndex
+      );
+    } else {
+      updatedCartItems[productInCartIndex].quantity -= 1;
+    }
+    setCartItems(updatedCartItems);
   };
 
   return (
     <div>
       <Header openCart={openCart} />
       <Products onAddToCart={handleAddToCart} />
-      <Cart showCart={showCart} closeCart={closeCart} cartItems={cartItems} />
+      <Cart
+        showCart={showCart}
+        closeCart={closeCart}
+        cartItems={cartItems}
+        onIncQuantity={handleIncreaseQuantity}
+        onDecQuantity={handleDecreaseQuantity}
+      />
     </div>
   );
 }
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import Modal from "../UI/Modal";
 import "./Cart.css";
 
-function CartItem({ id, name, image, quantity }) {
+function CartItem({ id, name, image, quantity, onIncQuantity, onDecQuantity }) {
   return (
     <div className="cart-item">
       <div className="item-img">
@@ -12,10 +12,20 @@ function CartItem({ id, name, image, quantity }) {
         <div className="item-qty">
           <div>Qty: {quantity}</div>
           <div>
-            <button className="yellow-button qty-button qty-plus-button">+</button>
+            <button
+              className="yellow-button qty-button qty-plus-button"
+              onClick={() => onIncQuantity(id)}
+            >
+              +
+            </button>
           </div>
           <div>
-            <button className="yellow-button qty-button">-</button>
+            <button
+              className="yellow-button qty-button"
+              onClick={() => onDecQuantity(id)}
+            >
+              -
+            </button>
           </div>
         </div>
       </div>
@@ -23,27 +33,41 @@ function CartItem({ id, name, image, quantity }) {
   );
 }
 
-function Cart({ showCart, closeCart, cartItems }) {
+function Cart({
+  showCart,
+  closeCart,
+  cartItems,
+  onIncQuantity,
+  onDecQuantity,
+}) {
   return (
     <Modal show={showCart} onClose={closeCart}>
       <div className="cart-container">
         <div className="cart-heading">Cart</div>
-        {cartItems.map((item) => (
-          <CartItem
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            image={item.image}
-            quantity={item.quantity}
-          />
-        ))}
+        {cartItems.length > 0 ? (
+          cartItems.map((item) => (
+            <CartItem
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              quantity={item.quantity}
+              onIncQuantity={onIncQuantity}
+              onDecQuantity={onDecQuantity}
+            />
+          ))
+        ) : (
+          <div className="empty-cart">Cart is empty</div>
+        )}
         <div className="cart-buttons">
-            <button className="black-button close-cart" onClick={closeCart}>
-                Close
-            </button>
+          <button className="black-button close-cart" onClick={closeCart}>
+            Close
+          </button>
+          {cartItems.length > 0 && (
             <button className="yellow-button" onClick={closeCart}>
-                Checkout
+              Checkout
             </button>
+          )}
         </div>
       </div>
     </Modal>
